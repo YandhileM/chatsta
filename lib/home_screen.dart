dceb59777c32771f2e3edc77efa16c0040463e83
Yandile <yandhilemaluleke@gmail.com>
2025-01-07 16:46:30 +0200

    Enhance home screen layout with additional greeting message and edit button

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,28 +11,54 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
         backgroundColor: Colors.white,
-        centerTitle: false,
-        title: const Column(children: [
-          Text(
-            'Hello Yandhile',
-            style: TextStyle(
-              color: Colors.grey,
-              fontWeight: FontWeight.w300,
-              fontSize: 12,
-            ),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          title: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hello Yandhile',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 12,
+                ),
+              ),
+              Text(
+                'Chatsta message',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ],
           ),
-          Text(
-            'Chatsta message',
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          )
-        ]),
-      ),
-    );
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.edit_square,
+                  // CupertinoIcons.pencil_circle_fill,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, int i) {
+                    return Container();
+                  })
+            ],
+          ),
+        ));
   }
 }
