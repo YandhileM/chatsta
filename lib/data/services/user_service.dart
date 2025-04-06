f7be0c3e76148f030cf0efbe350c302e576ee37a
Yandile <yandhilemaluleke@gmail.com>
2025-04-06 13:06:16 +0200

    Add currentUsername and secret parameters to fetchUsers method in UserService

--- lib/data/services/user_service.dart
+++ lib/data/services/user_service.dart
@@ -6,13 +6,16 @@ import '../../core/constants/api_constants.dart';
 class UserService {
   final String baseUrl = ApiConstants.baseUrl;
   final String privateKey = ApiConstants.privateKey;
+  
 
-  Future<List<Map<String, dynamic>>> fetchUsers() async {
+  Future<List<Map<String, dynamic>>> fetchUsers(currentUsername, secret) async {
     try {
       final response = await http.get(
         Uri.parse('$baseUrl/users/'),
         headers: {
           'Project-ID': privateKey,
+           'User-Name': currentUsername,
+          'User-Secret': secret,
         },
       );
 
