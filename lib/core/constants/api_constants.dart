de98fa69ef8c3c0d2c480b8c9133c56648ccd310
Yandhile <yandhilemaluleke@gmail.com>
2025-06-19 08:37:25 +0200

    Fix privateKey assignment in ApiConstants to use PROJECT_ID from environment variables

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
   static String baseUrl = dotenv.env['BASE_URL'] ?? '';
-  static String privateKey = dotenv.env['PRIVATE_KEY'] ?? '';
+  static String privateKey = dotenv.env['PROJECT_ID'] ?? '';
   static String projectId = dotenv.env['PROJECT_ID'] ?? '';
 }
